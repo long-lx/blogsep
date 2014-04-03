af46bdf5fd49fcf4e4a0d45a46f93923876142d0
long-lx <long-lx@septeni-technology.jp>
2014-04-03 17:03:00 +0700

    add config

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@ define('DB_NAME', 'blog');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '192.168.1.55');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
